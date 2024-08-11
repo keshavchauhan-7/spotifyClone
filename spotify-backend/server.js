1a700e858836f8df07ed14dba361fb2426618e7e
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-11 22:16:55 +0530

    create backend

---
+++ spotify-backend/server.js
@@ -0,0 +1,7 @@
+// cloudinary : we can store the image and mp3 files
+// cors : we can connect the backend and frontend
+// dotenv : that will allow us to store the enviroment variable
+// express : to create the backend api's
+// mongoose : to connect with the database and manage our database 
+// multer : we can upload the image and mp3 files that we will get from the frontend 
+// nodemon : that we can create one script that will easily run our project 
