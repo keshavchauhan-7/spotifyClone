06b9655c0017fb19519f723f7130f543494fabc1
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-14 22:08:09 +0530

    create backend server

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -5,3 +5,23 @@
 // mongoose : to connect with the database and manage our database 
 // multer : we can upload the image and mp3 files that we will get from the frontend 
 // nodemon : that we can create one script that will easily run our project 
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// initializing routes
+app.get('/', (req, res) => res.send(" API Working"))
+
+app.listen(port, () => console.log(`Server started on ${port}`))
