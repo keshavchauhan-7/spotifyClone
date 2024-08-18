a18f7fcf076bcff4f6083b315f8eaf42c4205669
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-18 12:59:21 +0530

    create admin panel

--- spotify-backend/src/controllers/albumController.js
+++ spotify-backend/src/controllers/albumController.js
@@ -30,11 +30,23 @@ const addAlbum = async (req, res) => {
 }
 
 const listAlbum = async (req, res) => {
+    try {
+        const allAlbums = await albumModel.find({});
+        res.json({ success: true, albums: allAlbums })
+    } catch (error) {
+        res.json({ success: false })
+
+    }
 
 }
 
 const removeAlbum = async (req, res) => {
-
+    try {
+        await albumModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Album Removed" })
+    } catch (error) {
+        res.json({ success: false })
+    }
 }
 
 export { addAlbum, listAlbum, removeAlbum }
