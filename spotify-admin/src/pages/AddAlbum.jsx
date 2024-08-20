5f7176a61bfa6f2c58a6a6c668f91f7a818e5c96
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-20 10:12:20 +0530

    create add album page

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AddAlbum = () => {
     return (
-        <div>
-        </div>
+        <form className='flex flex-col items-start gap-8 text-gray-600'>
+            <div className='flex flex-col gap-4'>
+                <p>Upload Image</p>
+                <input type="file" id='image' accept='image/*' hidden />
+                <label htmlFor="image" />
+                <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+            </div>
+
+            <div className='flex flex-col gap-2.5'>
+                <p>Album Name</p>
+                <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' type="text" placeholder='Type here' />
+            </div>
+        </form>
     )
 }
 
